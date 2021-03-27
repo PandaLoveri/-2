24ed986a578a0ec49fd5ce70c48cc065a246e7af
ILSIYA <ilsiya.jakupova@yandex.ru>
2021-03-27 15:43:20 +0500

    Добавила утиный манок

---
+++ DuckSwim/DuckDecoy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DuckSwim.behaviors;
+
+namespace DuckSwim
+{
+    class DuckDecoy
+    {
+        public void PerformQuack()
+        {
+            Quackle quackleObject = new Quackle();
+            quackleObject.Quack();
+        }
+    }
+}
