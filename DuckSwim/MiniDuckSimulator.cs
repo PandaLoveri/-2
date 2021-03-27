c2c821c10ad3e21025e1b1a94859d4c57ccb3fd1
ILSIYA <ilsiya.jakupova@yandex.ru>
2021-03-27 23:19:16 +0500

    Добавлены в пруд MiniDuckSimulator новые утки: жаренная и деревянная

--- DuckSwim/MiniDuckSimulator.cs
+++ DuckSwim/MiniDuckSimulator.cs
@@ -9,8 +9,25 @@ namespace DuckSwim
         static void Main(string[] args)
         {
             Duck mallard = new MallardDuck();
+            mallard.Display();
             mallard.PerformFly();
             mallard.PerformQuack();
+            mallard.PerformSwim();
+            Console.WriteLine();
+
+            RoastDuck roast = new RoastDuck();
+            roast.Display();
+            roast.PerformFly();
+            roast.PerformQuack();
+            roast.PerformSwim();
+            Console.WriteLine();
+
+            WoodenDuck wooden = new WoodenDuck();
+            wooden.Display();
+            wooden.PerformFly();
+            wooden.PerformQuack();
+            wooden.PerformSwim();
+
         }
     }
 }
